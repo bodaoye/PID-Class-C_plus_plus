11d0caf61a0322549f7c364b23d4b3da7876b86c
JFG <73979026+bodaoye@users.noreply.github.com>
2023-05-15 02:54:48 +0800

    feat:initial commit.

---
+++ pid.cpp
@@ -0,0 +1,85 @@
+#include "warrior_common/pid.hpp"
+#include <iostream>
+PID::PID(uint32_t mode,
+		 uint32_t maxout,
+		 uint32_t intergral_limit,
+		 float kp,
+		 float ki,
+		 float kd)
+{
+	IntegralLimit_ = intergral_limit;
+	MaxOutput_ = maxout;
+	pid_mode_ = mode;
+
+	P = kp;
+	I = ki;
+	D = kd;
+}
+
+void PID::Init(
+	uint32_t mode,
+	uint32_t maxout,
+	uint32_t intergral_limit,
+	float kp,
+	float ki,
+	float kd)
+{
+	IntegralLimit_ = intergral_limit;
+	MaxOutput_ = maxout;
+	pid_mode_ = mode;
+
+	P = kp;
+	I = ki;
+	D = kd;
+}
+
+void PID::pid_reset(float kp,
+					float ki,
+					float kd)
+{
+	P = kp;
+	I = ki;
+	D = kd;
+}
+
+float PID::pid_calc(float get, float set)
+{
+	get_[NOW] = get;
+	set_[NOW] = set;
+	err_[NOW] = set - get; // set - measure
+	if (max_err_ != 0 && fabs(err_[NOW]) > max_err_)
+		return 0;
+	if (deadband_ != 0 && fabs(err_[NOW]) < deadband_)
+		return 0;
+
+	if (pid_mode_ == POSITION_PID) // 位置式p
+	{
+		pout_ = P * err_[NOW];
+		iout_ += I * err_[NOW];
+		dout_ = D * (err_[NOW] - err_[LAST]);
+		abs_limit(&(iout_), IntegralLimit_, 0);
+		pos_out_ = pout_ + iout_ + dout_;
+		abs_limit(&(pos_out_), MaxOutput_, 0);
+		last_pos_out_ = pos_out_; // update last time
+	}
+	else if (pid_mode_ == DELTA_PID) // 增量式P
+	{
+		pout_ = P * (err_[NOW] - err_[LAST]);
+		iout_ = I * err_[NOW];
+		dout_ = D * (err_[NOW] - 2 * err_[LAST] + err_[LLAST]);
+
+		abs_limit(&(iout_), IntegralLimit_, 0);
+		delta_u_ = pout_ + iout_ + dout_;
+		delta_out_ = last_delta_out_ + delta_u_;
+		abs_limit(&(delta_out_), MaxOutput_, 0);
+		last_delta_out_ = delta_out_; // update last time
+	}
+
+	err_[LLAST] = err_[LAST];
+	err_[LAST] = err_[NOW];
+	get_[LLAST] = get_[LAST];
+	get_[LAST] = get_[NOW];
+	set_[LLAST] = set_[LAST];
+	set_[LAST] = set_[NOW];
+	return pid_mode_ == POSITION_PID ? pos_out_ : delta_out_;
+}
---
+++ pid.hpp
@@ -0,0 +1,95 @@
+#ifndef __PID_H__
+#define __PID_H__
+#include <cstdint>
+#include <cmath>
+/*
+------------------------------------------------------------------------------
+~ File   : pid.h
+~ Author : Fengguang Ji
+~ Version: V1.1.0
+~ Created: 05/15/2023 00:51:00 AM
+~ Brief  :
+~ Support:
+		  E-Mail : jfg1765@163.com
+
+		  Github : https://github.com/Majid-Derhambakhsh
+------------------------------------------------------------------------------
+~ Description:      CPP version
+
+~ Attention  :
+
+~ Changes    :
+------------------------------------------------------------------------------
+*/
+
+/* PID Mode */
+enum
+{
+	LLAST = 0,
+	LAST = 1,
+	NOW = 2,
+
+	POSITION_PID,
+	DELTA_PID,
+};
+
+class PID
+{
+
+private:
+
+
+	float set_[3]; // 目标值,包含NOW， LAST， LLAST上上次
+	float get_[3]; // 测量值
+	float err_[3]; // 误差
+
+	float pout_; // p输出
+	float iout_; // i输出
+	float dout_; // d输出
+
+	float pos_out_;		 // 本次位置式输出
+	float last_pos_out_; // 上次输出
+	float delta_u_;		 // 本次增量值
+	float delta_out_;	 // 本次增量式输出 = last_delta_out + delta_u
+	float last_delta_out_;
+
+	float max_err_;
+	float deadband_; // err < deadband return
+	uint32_t pid_mode_;
+	uint32_t MaxOutput_;	 // 输出限幅
+	uint32_t IntegralLimit_; // 积分限幅
+
+public:
+
+	double P;
+	double I;
+	double D;
+
+	PID(uint32_t mode,
+		uint32_t maxout,
+		uint32_t intergral_limit,
+		float kp,
+		float ki,
+		float kde);
+	void Init(uint32_t mode,
+			  uint32_t maxout,
+			  uint32_t intergral_limit,
+			  float kp,
+			  float ki,
+			  float kd);
+
+	/*change PID parameters when the program running...*/
+	void pid_reset(float kp,
+				   float ki,
+				   float kd);
+	float pid_calc(float get, float set);
+	void abs_limit(float *a, float ABS_MAX, float offset)
+	{
+		if (*a > ABS_MAX + offset)
+			*a = ABS_MAX + offset;
+		if (*a < -ABS_MAX + offset)
+			*a = -ABS_MAX + offset;
+	}
+};
+#endif
+
